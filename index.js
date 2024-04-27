23d76ce40a043663e106e5ab174bf503a80a2b74
Abhishek kushwah <113005763+abhishek42kushwah@users.noreply.github.com>
2024-04-27 16:21:36 +0530

    adding cros npm

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser")
 const authRoutes = require("./routes/authRoutes") 
 const userRoutes = require("./routes/userRoutes") 
 const connection = require("./config")
+const cors = require("cors");
  const app = express()
 
  port = 4000 
@@ -11,7 +12,19 @@ const connection = require("./config")
 app.use(bodyParser.json());
 app.use("/auth",authRoutes);
 app.use("/user",userRoutes) 
+app.use(
+   cors({
+       origin:"http://localhost:3000",
+       credentials:true,
+   })
+)
 
+app.get("/",(req,res)=>{
+   return res.json({
+       success:true,
+       message:"Your server is up running..."
+   })
+})
 
  app.listen(port,()=>{
     console.log(`app start ${port} Number `)
